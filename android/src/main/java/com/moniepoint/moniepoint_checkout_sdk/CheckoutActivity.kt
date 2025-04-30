05a39b2e619c6bbbc6f31591b5b7d81c1c08c462
JosephKilasi <joseph.kilasi@moniepoint.com>
2025-04-30 21:08:32 +0300

    add support for 3ds once payment token is generated

--- android/src/main/java/com/moniepoint/moniepoint_checkout_sdk/CheckoutActivity.kt
+++ android/src/main/java/com/moniepoint/moniepoint_checkout_sdk/CheckoutActivity.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.checkout.base.model.CardScheme
 import com.checkout.base.model.Environment
@@ -16,14 +17,27 @@ import com.checkout.frames.screen.paymentform.model.PaymentFormConfig
 import com.checkout.frames.style.component.base.ContainerStyle
 import com.checkout.frames.style.screen.PaymentDetailsStyle
 import com.checkout.frames.style.screen.PaymentFormStyle
+import com.checkout.threeds.AuthenticationCallback
+import com.checkout.threeds.domain.model.AuthenticationCompleted
+import com.checkout.threeds.domain.model.AuthenticationError
+import com.checkout.threeds.domain.model.AuthenticationErrorType
+import com.checkout.threeds.domain.model.AuthenticationResult
+import com.checkout.threeds.domain.model.ResultType
+import com.checkout.threedsecure.model.ThreeDSRequest
+import com.checkout.threedsecure.model.ThreeDSResult
+import com.checkout.threedsecure.model.ThreeDSResultHandler
 import com.checkout.tokenization.model.TokenDetails
+import com.moniepoint.moniepoint_checkout_sdk.lib.PaymentUtil
+import com.moniepoint.moniepoint_checkout_sdk.lib.PaymentUtil.*
 
- class CheckoutActivity : AppCompatActivity() {
+class CheckoutActivity : AppCompatActivity(), AuthenticationCallback {
+    var paymentFormMediator: PaymentFormMediator? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val publicKey = intent.getStringExtra(EXTRA_PUBLIC_KEY) ?: return finishWithError("Missing public key")
+        setContentView(R.layout.activity_checkout)
+        val publicKey =
+            intent.getStringExtra(EXTRA_PUBLIC_KEY) ?: return finishWithError("Missing public key")
         val envRaw = intent.getIntExtra(EXTRA_ENVIRONMENT, CheckoutEnvironment.SANDBOX.raw)
         val environment = when (CheckoutEnvironment.ofRaw(envRaw)) {
             CheckoutEnvironment.PRODUCTION -> Environment.PRODUCTION
@@ -41,7 +55,8 @@ import com.checkout.tokenization.model.TokenDetails
                     putExtra(EXTRA_TOKEN, tokenDetails.token)
                 }
                 setResult(RESULT_OK, resultIntent)
-                finish()
+                purchase(tokenDetails.token)
+                //finish()
             }
 
             override fun onFailure(errorMessage: String) {
@@ -63,8 +78,32 @@ import com.checkout.tokenization.model.TokenDetails
             style = PaymentFormStyle(),
             supportedCardSchemeList = listOf(CardScheme.VISA, CardScheme.MASTERCARD),
         )
+        paymentFormMediator = PaymentFormMediator(paymentFormConfig);
+        paymentFormMediator?.setActivityContent(this)
 
-        PaymentFormMediator(paymentFormConfig).setActivityContent(this)
+    }
+
+    private fun purchase(token: String) {
+        createPayment(token) { success, redirectUrl ->
+
+            if (redirectUrl != null) {
+                start3DS(redirectUrl)
+            } else {
+                println(" no redirect url avaulable")
+            }
+        }
+    }
+
+    private fun start3DS(redirectUrl: String) {
+        val threeDSRequest = ThreeDSRequest(
+            this.findViewById(R.id.frameMe),
+            redirectUrl,
+            "https://authentication-devices.sandbox.checkout.com/sessions-interceptor/sid_7ul6fdirserevaifrdsir3cexu",
+            "https://authentication-devices.sandbox.checkout.com/sessions-interceptor/sid_7ul6fdirserevaifrdsir3cexu",
+            threeDSResultHandler
+        )
+
+        paymentFormMediator?.handleThreeDS(threeDSRequest)
     }
 
     private fun finishWithError(message: String) {
@@ -90,5 +129,57 @@ import com.checkout.tokenization.model.TokenDetails
         }
 
         const val REQUEST_CODE = 4242
+    }
+
+    override fun onAuthenticationResult(authenticationResult: AuthenticationResult) {
+        when (authenticationResult.resultType) {
+            ResultType.Completed -> {
+                // Handle authentication result - continue with the payment based on transaction status
+
+                val authenticationCompleted: AuthenticationCompleted =
+                    (authenticationResult as AuthenticationCompleted)
+                val sdkTransactionId = authenticationCompleted.sdkTransactionId
+                val transactionStatus = authenticationCompleted.transactionStatus
+                println("Result: Authentication success: $sdkTransactionId")
+            }
+
+            ResultType.Error -> {
+                // Handle error (result as AuthenticationError)
+
+                // Handle error based on error type category
+                val errorType: AuthenticationErrorType =
+                    (authenticationResult as AuthenticationError).errorType
+
+                // Handle error based on fine grained error code or simply log the error
+                val errorCode: String = (authenticationResult as AuthenticationError).errorCode
+                println("Result: Authentication error: $errorCode $errorType")
+            }
+        }
+
+    }
+
+    private val threeDSResultHandler: ThreeDSResultHandler = { threeDSResult: ThreeDSResult ->
+        when (threeDSResult) {
+            is ThreeDSResult.Success -> {
+                /* Handle success result */
+                val token: String = (threeDSResult as ThreeDSResult.Success).token
+                //displayMessage("Result", "Authentication success: $token", true)
+                println("Result: Authentication success: $token")
+                threeDSResult.token
+            }
+
+            is ThreeDSResult.Failure -> {
+                val errorMessage: String = (threeDSResult as Error).message.toString()
+                println("Result Authentication error:  + $errorMessage, true")
+                /* Handle failure result */
+            }
+
+            is ThreeDSResult.Error -> {
+                /* Handle error result */
+                println("Result Authentication Failure");
+                threeDSResult.error
+            }
+        }
+
     }
 }
---
+++ android/src/main/kotlin/com/moniepoint/moniepoint_checkout_sdk/lib/PaymentUtil.java
@@ -0,0 +1,129 @@
+package com.moniepoint.moniepoint_checkout_sdk.lib;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class PaymentUtil {
+    private static final boolean LOGGING_ENABLED = true;
+
+    public interface Callback {
+        void onPaymentCreated(boolean success, @Nullable String redirectUrl);
+    }
+
+    public static OkHttpClient newClient() {
+        OkHttpClient.Builder okHttpBuilder = new OkHttpClient.Builder();
+
+        if (LOGGING_ENABLED) {
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor(message -> Log.d("okhttp", message));
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            okHttpBuilder.addInterceptor(loggingInterceptor);
+        }
+
+        return okHttpBuilder.build();
+    }
+
+    public static void createPayment(String token, Callback callback) {
+
+        Handler responseHandler = new Handler(Looper.getMainLooper());
+        RequestBody requestBody = buildPaymentRequestBody(token);
+        Request paymentRequest = new Request.Builder()
+                .url("https://api.sandbox.checkout.com/payments")
+
+                .addHeader("Authorization", "Bearer " + "sk_sbox_qxpuwds47t52ltjizlnrgke5imw")
+                .post(requestBody)
+                .build();
+
+        newClient().newCall(paymentRequest)
+                .enqueue(new okhttp3.Callback() {
+
+                    private void postResponse(boolean success, @Nullable String redirectUrl) {
+                        responseHandler.post(() -> callback.onPaymentCreated(success, redirectUrl));
+                    }
+
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        e.printStackTrace();
+                        postResponse(false, null);
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) {
+                        boolean result = false;
+                        String redirectUrl = null;
+                        try {
+                            ResponseBody body = response.body();
+                            if (response.isSuccessful() && body != null) {
+                                String jsonString = body.string();
+                                JSONObject jsonObject = new JSONObject(jsonString);
+                                String paymentStatus = jsonObject.optString("status");
+                                switch (paymentStatus) {
+                                    case "Pending":
+                                    case "Declined":
+                                        result = false;
+                                        break;
+                                    default:
+                                        result = true;
+                                        break;
+                                }
+
+                                JSONObject linksObject = jsonObject.optJSONObject("_links");
+                                if (linksObject != null) {
+                                    JSONObject redirectObject = linksObject.optJSONObject("redirect");
+                                    if (redirectObject != null) {
+                                        redirectUrl = redirectObject.getString("href");
+                                    }
+                                }
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        } finally {
+                            postResponse(result, redirectUrl);
+                        }
+                    }
+                });
+    }
+
+    private static RequestBody buildPaymentRequestBody(String token) {
+
+        String requestBodyJson = "{\n" +
+                "    \"source\": {\n" +
+                "        \"type\": \"token\",\n" +
+                "        \"token\": \"" + token + "\"\n" +
+                "    },\n" +
+                "    \"amount\": " + "100" + ",\n" +
+                "    \"currency\": \"GBP\",\n" +
+                "    \"reference\": \"ORD-5023-4E89\",\n" +
+                "    \"processing_channel_id\": \"pc_mvcoe5akpatufhgaq7sofug4qu\",\n" +
+                "    \"success_url\": \"" + "https://example.com/payments/success" + "\",\n" +
+                "    \"failure_url\": \"" + "https://example.com/payments/success" + "\",\n" +
+                "    \"3ds\": {\n" +
+                "        \"enabled\": true,\n" +
+                "        \"challenge_indicator\": \"challenge_requested_mandate\",\n" +
+
+                "    }\n" +
+                "}";
+
+        return RequestBody.create(
+                requestBodyJson,
+                MediaType.parse("application/json; charset=utf-8")
+        );
+    }
+}
